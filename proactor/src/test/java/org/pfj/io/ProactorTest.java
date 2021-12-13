5f1312e18fc5378d8b35a9a046d0d8d48ce0227b
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2021-12-13 11:32:14 +0100

    Restore more proactor tests

--- proactor/src/test/java/org/pfj/io/ProactorTest.java
+++ proactor/src/test/java/org/pfj/io/ProactorTest.java
@@ -19,11 +19,14 @@ package org.pfj.io;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.pfj.io.async.common.OffsetT;
+import org.pfj.io.async.common.SizeT;
+import org.pfj.io.async.file.FileDescriptor;
 import org.pfj.io.async.file.FilePermission;
 import org.pfj.io.async.file.OpenFlags;
 import org.pfj.io.async.net.*;
 import org.pfj.io.async.util.OffHeapBuffer;
 import org.pfj.io.scheduler.Timeout;
+import org.pfj.lang.Option;
 import org.pfj.lang.Result;
 import org.pfj.io.async.net.AddressFamily;
 import org.pfj.io.async.net.InetPort;
@@ -31,17 +34,21 @@ import org.pfj.io.async.net.SocketAddressIn;
 import org.pfj.io.async.net.SocketFlag;
 import org.pfj.io.async.net.SocketOption;
 import org.pfj.io.async.net.SocketType;
+import org.pfj.lang.Unit;
 
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.EnumSet;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.pfj.lang.Option.empty;
+import static org.pfj.lang.Option.option;
 
 //TODO: remaining tests
 class ProactorTest {
@@ -50,117 +57,126 @@ class ProactorTest {
     @Test
     void nopCanBeSubmitted() {
         final var finalResult = new AtomicReference<Result<?>>();
-//        final var promise = proactor.nop(Promise.promise())
-//                                    .onResult(Assertions::assertNotNull)
-//                                    .onResult(System.out::println)
-//                                    .onResult(finalResult::set);
-//        waitForResult(promise);
-//        finalResult.get().onFailure($ -> fail());
+
+        proactor.nop((result, submitter) -> finalResult.set(result));
+
+        waitForResult(finalResult);
+
+        finalResult.get()
+            .onSuccess(Assertions::assertNotNull)
+            .onSuccess(System.out::println)
+            .onFailure(__ -> fail());
     }
 
-//    @Test
-//    void delayCanBeSubmitted() {
-//        final var finalResult = new AtomicReference<Result<Duration>>();
-//        proactor.delay((result, submitter) -> finalResult.set(result), Timeout.timeout(100).millis());
-//        do {
-//            proactor.processIO(); //For submission
-//            proactor.processIO(); //For completion
-//        } while (finalResult.get() == null);
-//
-//        finalResult.get()
-//                   .onFailure($ -> fail())
-//                   .onSuccess(duration -> assertTrue(duration.compareTo(Duration.ofMillis(100)) > 0));
-//    }
-//
-//    @Test
-//    void fileCanBeOpenedAndClosed() {
-//        final var finalResult = new AtomicReference<Result<?>>();
-//        final var promise = proactor.open(Path.of("target/classes/org/reactivetoolbox/io/Proactor.class"),
-//                                          EnumSet.of(OpenFlags.READ_ONLY),
-//                                          EnumSet.noneOf(FilePermission.class),
-//                                          empty())
-//                                    .onResult(System.out::println)
-//                                    .onResult(v -> v.onSuccess(fd -> assertTrue(fd.descriptor() > 0))
-//                                                    .onFailure(f -> fail()))
-//                                    .flatMap(fd -> proactor.closeFileDescriptor(fd, empty()))
-//                                    .onResult(System.out::println)
-//                                    .onResult(finalResult::set);
-//
-//        waitForResult(promise);
-//        finalResult.get().onFailure($ -> fail());
-//    }
-//
-//    @Test
-//    void fileCanBeOpenedReadAndClosed() {
-//        final var finalResult = new AtomicReference<Result<?>>();
-//        try (final OffHeapBuffer buffer = OffHeapBuffer.fixedSize(1024 * 1024)) {
-//            final var promise = proactor.open(Path.of("target/classes/org/reactivetoolbox/io/Proactor.class"),
-//                                              EnumSet.of(OpenFlags.READ_ONLY),
-//                                              EnumSet.noneOf(FilePermission.class),
-//                                              empty())
-//                                        .onResult(System.out::println)
-//                                        .onResult(v -> v.onSuccess(fd -> assertTrue(fd.descriptor() > 0))
-//                                                        .onFailure(f -> fail()))
-//                                        .flatMap(fd1 -> proactor.read(fd1, buffer, OffsetT.ZERO, empty()).map(sz -> tuple(fd1, sz)))
-//                                        .onResult(System.out::println)
-//                                        .flatMap(fdSz -> fdSz.map((fd, sz) -> proactor.closeFileDescriptor(fd, empty())))
-//                                        .onResult(System.out::println)
-//                                        .onResult(v -> v.onFailure(f -> fail()))
-//                                        .onResult(finalResult::set);
-//
-//            waitForResult(promise);
-//            finalResult.get().onFailure($ -> fail());
-//        }
-//    }
-//
-//    @Test
-//    void externalHostCanBeConnectedAndRead() throws UnknownHostException {
-//        final var finalResult = new AtomicReference<Result<?>>();
-//        final var addr = java.net.Inet4Address.getByName("www.google.com");
-//        final var address = Inet4Address.inet4Address(addr.getAddress()).fold($ -> fail(),
-//                                                                 inetAddress -> SocketAddressIn.create(InetPort.inetPort(80),
-//                                                                                                       inetAddress));
-//
-//        System.out.println("Address: " + address);
-//
-//        try (final OffHeapBuffer preparedText = OffHeapBuffer.fromBytes("GET /\n".getBytes(StandardCharsets.US_ASCII))) {
-//            try (final OffHeapBuffer buffer = OffHeapBuffer.fixedSize(256)) {
-//                buffer.clear().used(buffer.size());
-//                final var promise = proactor.socket(AddressFamily.INET,
-//                                                    SocketType.STREAM,
-//                                                    SocketFlag.none(),
-//                                                    SocketOption.reuseAll())
-//                                            .onResult(r1 -> System.out.println("Socket created: " + r1))
-//                                            .flatMap(fd -> proactor.connect(Promise.promise(), fd, address, option(Timeout.timeout(1).seconds()))
-//                                                                   .onFailure($ -> proactor.closeFileDescriptor(fd, empty()))
-//                                                                   .map(sz -> fd))
-//                                            .onResult(r1 -> System.out.println("Socket connected: " + r1))
-//                                            .flatMap(fd -> proactor.write(fd, preparedText, OffsetT.ZERO, option(Timeout.timeout(1).seconds()))
-//                                                                   .map(sz -> fd))
-//                                            .flatMap(fd -> proactor.read(fd, buffer, OffsetT.ZERO, option(Timeout.timeout(1).seconds()))
-//                                                                   .onFailure($ -> proactor.closeFileDescriptor(fd, empty()))
-//                                                                   .map(sz -> fd))
-//                                            .onResult(System.out::println)
-//                                            .flatMap(fd -> proactor.closeFileDescriptor(fd, empty()))
-//                                            .onResult(finalResult::set);
-//                waitForResult(promise);
-//                finalResult.get().onFailure($ -> fail());
-//
-//                System.out.println("Content: [" + new String(buffer.export(), StandardCharsets.UTF_8) + "]");
-//            }
-//        }
-//    }
+    @Test
+    void delayCanBeSubmitted() {
+        final var finalResult = new AtomicReference<Result<Duration>>();
+        proactor.delay((result, submitter) -> finalResult.set(result), Timeout.timeout(100).millis());
 
-//    private void waitForResult(final Promise<?> promise) {
-//        final AtomicBoolean ready = new AtomicBoolean(false);
-//
-//        promise.onResult(v -> ready.lazySet(true));
-//
-//        do {
-//            proactor.processIO(); //For submission
-//            proactor.processIO(); //For completion
-//        } while (!ready.get());
-//
-//        promise.syncWait();
-//    }
+        waitForResult(finalResult);
+
+        finalResult.get()
+            .onFailure(failure -> fail(failure::message))
+            .onSuccess(duration -> System.out.println("Duration: " + duration.toMillis() + " milliseconds"))
+            .onSuccess(duration -> assertTrue(duration.compareTo(Duration.ofMillis(100)) > 0));
+    }
+
+    @Test
+    void fileCanBeOpenedAndClosed() {
+        final var fileDescriptor = new AtomicReference<Result<FileDescriptor>>();
+
+        proactor.open((result, submitter) -> fileDescriptor.set(result),
+            Path.of("target/classes/org/pfj/io/Proactor.class"),
+            EnumSet.of(OpenFlags.READ_ONLY),
+            EnumSet.noneOf(FilePermission.class),
+            empty());
+
+        waitForResult(fileDescriptor);
+
+        fileDescriptor.get()
+            .onSuccess(fd -> System.out.println("Open successful: " + fd))
+            .onSuccess(fd -> assertTrue(fd.descriptor() > 0))
+            .onFailure(f -> fail(f::message));
+
+        final var closeResult = new AtomicReference<Result<Unit>>();
+        fileDescriptor.get()
+            .onSuccess(fd -> proactor.closeFileDescriptor(((result, submitter) -> closeResult.set(result)), fd, empty()));
+
+        waitForResult(closeResult);
+
+        closeResult.get()
+            .onSuccess(unit -> System.out.println("Close successful " + unit))
+            .onFailure(f -> fail(f::message));
+    }
+
+    @Test
+    void externalHostCanBeConnectedAndRead() throws UnknownHostException {
+        final var addr = java.net.Inet4Address.getByName("www.google.com");
+        final var address = Inet4Address.inet4Address(addr.getAddress())
+            .fold(failure -> fail(failure::message),
+                inetAddress -> SocketAddressIn.create(InetPort.inetPort(80),
+                    inetAddress));
+
+        System.out.println("Address: " + address);
+
+        try (final OffHeapBuffer preparedText = OffHeapBuffer.fromBytes("GET /\n".getBytes(StandardCharsets.US_ASCII))) {
+            try (final OffHeapBuffer buffer = OffHeapBuffer.fixedSize(768)) {
+                buffer.clear().used(buffer.size());
+
+                var socketResult = new AtomicReference<Result<FileDescriptor>>();
+                proactor.socket((result, submitter) -> socketResult.set(result),
+                    AddressFamily.INET,
+                    SocketType.STREAM,
+                    SocketFlag.none(),
+                    SocketOption.reuseAll());
+
+                waitForResult(socketResult);
+                socketResult.get()
+                    .onFailure(failure -> Assertions.fail(failure::message))
+                    .onSuccess(fd -> {
+                        var connectResult = new AtomicReference<Result<FileDescriptor>>();
+
+                        proactor.connect((result, submitter) -> connectResult.set(result),
+                            fd, address, Option.option(Timeout.timeout(1).seconds()));
+
+                        waitForResult(connectResult);
+
+                        connectResult.get()
+                            .onFailure(failure -> fail(failure::message))
+                            .onSuccess(r1 -> System.out.println("Socket connected: " + r1));
+
+                        var writeResult = new AtomicReference<Result<SizeT>>();
+                        proactor.write(((result, submitter) -> writeResult.set(result)),
+                            fd, preparedText, OffsetT.ZERO, option(Timeout.timeout(1).seconds()));
+
+                        waitForResult(writeResult);
+
+                        writeResult.get()
+                            .onFailure(failure -> fail(failure::message))
+                            .onSuccess(sizeT -> System.out.println("Wrote " + sizeT + " bytes"));
+
+                        var readResult = new AtomicReference<Result<SizeT>>();
+
+                        proactor.read(((result, submitter) -> readResult.set(result)),
+                            fd, buffer, OffsetT.ZERO, option(Timeout.timeout(1).seconds()));
+
+                        waitForResult(readResult);
+
+                        var closeResult = new AtomicReference<Result<Unit>>();
+                        proactor.closeFileDescriptor(((result, submitter) -> closeResult.set(result)), fd, empty());
+
+                        waitForResult(closeResult);
+
+                        System.out.println("Content: [" + new String(buffer.export(), StandardCharsets.UTF_8) + "]");
+                    });
+            }
+        }
+    }
+
+    private void waitForResult(AtomicReference<?> reference) {
+        do {
+            proactor.processIO(); //For submission
+            proactor.processIO(); //For completion
+        } while (reference.get() == null);
+    }
 }
