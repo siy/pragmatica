c5892b2544ca2bfd0a884bb4b0be4c2927fff176
Sergiy Yevtushenko <sergiy@radixdlt.com>
2022-06-15 15:48:51 +0200

    Minor cleanups

--- examples/src/test/java/org/pragmatica/example/calculator/pfj/CalculatorTest.java
+++ examples/src/test/java/org/pragmatica/example/calculator/pfj/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SuppressWarnings("deprecation")
 class CalculatorTest {
     @Test
     void simple_expr_parsed_mul() {
